588abc0829a2b3b466b892e4ddb9b18d12902861
Abdul Adhil PK <65992809+adhilcodes@users.noreply.github.com>
2020-12-02 19:00:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+
+pickle_in = open("classifier.pkl", "rb")
+classifier = pickle.load(pickle_in)
+
+@app.route('/')
+def home():
+      return render_template("index.html")
+
+#Get method
+
+@app.route('/predict', methods=['POST'])
+def predict():
+      
+
+      in_features = [float(x) for x in request.form.values()]
+      final_features = [np.array(in_features)]
+      prediction = classifier.predict(final_features)
+      final_prediction = str(prediction)
+      
+      return render_template("index.html", prediction_text="Predicted value is {}".format(final_prediction))
+
+if __name__ == "__main__":
+      app.run(debug=True)
+
+#predict_note?variance=2&skewness=3&curtosis=2&entropy=1
